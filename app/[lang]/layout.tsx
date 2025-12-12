8e2a14c1eaaf9e22dde7f488bca012b086f5900a
canaya2002 <canaya917@gmail.com>
2025-12-12 13:13:20 -0600

    Ads

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -5,7 +5,6 @@ import AIChatButton from '../components/AIChatButton';
 import { translations, Language } from '../lib/translations';
 import Script from 'next/script';
 import { Analytics } from "@vercel/analytics/next";
-// 1. IMPORTAR SPEED INSIGHTS
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import '../globals.css'; 
 
@@ -152,6 +151,7 @@ export default async function LangLayout({ children, params }: Props) {
   return (
     <html lang={currentLang} suppressHydrationWarning>
       <head>
+        {/* 1. SCHEMA ORG */}
         <Script
           id="schema-org"
           type="application/ld+json"
@@ -159,9 +159,10 @@ export default async function LangLayout({ children, params }: Props) {
           strategy="beforeInteractive"
         />
         
+        {/* 2. GOOGLE ANALYTICS (GA4) - ID REAL IMPLEMENTADO */}
         <Script
           async
-          src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
+          src="https://www.googletagmanager.com/gtag/js?id=G-V5F8J8QMZ4"
           strategy="afterInteractive"
         />
         <Script
@@ -172,20 +173,67 @@ export default async function LangLayout({ children, params }: Props) {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-XXXXXXXXXX');
+              gtag('config', 'G-V5F8J8QMZ4'); 
+            `,
+          }}
+        />
+
+        {/* 3. META PIXEL (Facebook & Instagram) - ID REAL IMPLEMENTADO */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1679590710105917');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+
+        {/* 4. TIKTOK PIXEL - ID REAL IMPLEMENTADO */}
+        <Script
+          id="tiktok-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function (w, d, t) {
+                w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=i+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+                
+                ttq.load('CVERFVJC77U9L0C1P6O0');
+                ttq.page();
+              }(window, document, 'ttq');
             `,
           }}
         />
       </head>
+      
       <body suppressHydrationWarning>
+        {/* Fallback para Meta Pixel (si el usuario no tiene JS) */}
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1679590710105917&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
         <LanguageProvider initialLanguage={currentLang}>
           {children}
           <WhatsAppButton />
           <AIChatButton />
           
-          {/* Vercel Tools */}
+          {/* Herramientas de Vercel */}
           <Analytics />
-          {/* 2. AGREGAR EL COMPONENTE SPEED INSIGHTS */}
           <SpeedInsights />
           
         </LanguageProvider>
